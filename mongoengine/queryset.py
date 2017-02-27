conversocial/mongoengine

commit aa7cd5b564cdbfe15082eb476b19d8d27d0c64b8

Do not create a cursor at sort specification time

mongoengine/queryset.py
@@ -1149,7 +1149,6 @@ class QuerySet(object):
             key_list.append((key, direction))
 
         self._ordering = key_list
-        self._cursor.sort(key_list)
         return self
 
     def explain(self, format=False):

mongoengine/__init__.py
@@ -12,7 +12,7 @@ from signals import *
 __all__ = (document.__all__ + fields.__all__ + connection.__all__ +
            queryset.__all__ + signals.__all__)
 
-VERSION = (0, 6, 15)
+VERSION = (0, 6, 16)
 
 
 def get_version():

commit 71b78fc0b0eaa8e7383393aa52001115d617187e

fix: noticket - fixes broken cursor ordering.
aa7cd5b564cdbfe15082eb476b19d8d27d0c64b8 broke the following test:

======================================================================
FAIL: test_ordering (tests.queryset.QuerySetTest)
    Ensure default ordering is applied and can be overridden.
    ----------------------------------------------------------------------
    Traceback (most recent call last):
          File "/home/vagrant/conv_libs/mongoengine/tests/queryset.py",
          line 755, in test_ordering
              self.assertEqual(latest_post.title, "Blog Post #3")
          AssertionError: u'Blog Post #1' != 'Blog Post #3'

This is because we were no longer calling sort() on the cursor after
calling order_by.

mongoengine/queryset.py
@@ -498,7 +498,7 @@ class QuerySet(object):
             if self._read_preference:
                 collection = collection.with_options(
                     read_preference=self._read_preference)
-                
+
             self._cursor_obj = collection.find(
                 self._query, **self._cursor_args)
 
@@ -511,6 +511,7 @@ class QuerySet(object):
                 self._cursor_obj.sort(self._ordering)
             elif self._document._meta['ordering']:
                 self.order_by(*self._document._meta['ordering'])
+                self._cursor_obj.sort(self._ordering)
 
             if self._limit is not None:
                 self._cursor_obj.limit(self._limit)