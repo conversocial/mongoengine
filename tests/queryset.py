conversocial/mongoengine

commit 6cc87c2156a4ed94847c73c9a2445900677d5c1e

fix: stop evaluating cursor all over the place

mongoengine/queryset.py
@@ -971,10 +971,6 @@ class QuerySet(object):
 
         :param n: the maximum number of objects to return
         """
-        if n == 0:
-            self._cursor.limit(1)
-        else:
-            self._cursor.limit(n)
         self._limit = n
 
         # Return self to allow chaining
@@ -986,7 +982,6 @@ class QuerySet(object):
 
         :param n: the number of objects to skip before returning results
         """
-        self._cursor.skip(n)
         self._skip = n
         return self
 
@@ -1003,7 +998,6 @@ class QuerySet(object):
 
         .. versionadded:: 0.5
         """
-        self._cursor.hint(index)
         self._hint = index
         return self
 

mongoengine/__init__.py
@@ -12,7 +12,7 @@ from signals import *
 __all__ = (document.__all__ + fields.__all__ + connection.__all__ +
            queryset.__all__ + signals.__all__)
 
-VERSION = (0, 6, 16)
+VERSION = (0, 6, 17)
 
 
 def get_version():

commit f1c245471a8c15b6afcca6d2ab86efa6ed1779d5

fix: noticket - fixes broken limit/skip/hint handling
6cc87c2156a4ed94847c73c9a2445900677d5c1e introduced a breaking change
causing the following test to fail:

FAIL: test_clone (tests.queryset.QuerySetTest)
    Ensure that cloning clones complex querysets
    ----------------------------------------------------------------------
    Traceback (most recent call last):
          File "/home/vagrant/conv_libs/mongoengine/tests/queryset.py",
          line 2496, in test_clone
              self.assertEqual(test.count(), test2.count())
          AssertionError: 89 != 10

However, it was not the .clone() call that was failing. The cursor that
had *not* been cloned was not having the limit() call applied because
the pymongo cursor had already been created.

The above commit introduced a similar error for skip() and hint() calls.
The test introduced to test skip() breaks without the change in place,
however, I could not find a test for hint() but I have re-introduced the
previous behaviour.

mongoengine/queryset.py
@@ -972,6 +972,12 @@ class QuerySet(object):
 
         :param n: the maximum number of objects to return
         """
+        if self._cursor_obj is not None:
+            if n == 0:
+                self._cursor_obj.limit(1)
+            else:
+                self._cursor_obj.limit(n)
+
         self._limit = n
 
         # Return self to allow chaining
@@ -983,6 +989,9 @@ class QuerySet(object):
 
         :param n: the number of objects to skip before returning results
         """
+        if self._cursor_obj is not None and n:
+            self._cursor_obj.skip(n)
+
         self._skip = n
         return self
 
@@ -999,6 +1008,9 @@ class QuerySet(object):
 
         .. versionadded:: 0.5
         """
+        if self._cursor_obj is not None:
+            self._cursor_obj.hint(index)
+
         self._hint = index
         return self
 

tests/queryset.py
@@ -2497,6 +2497,46 @@ class QuerySetTest(unittest.TestCase):
 
         Number.drop_collection()
 
+    def test_modify_evaluated_query_with_limit(self):
+        class Number(Document):
+            n = IntField()
+
+        Number.drop_collection()
+
+        for i in xrange(1, 101):
+            t = Number(n=i)
+            t.save()
+
+        test = Number.objects
+        self.assertEqual(test.count(), 100)
+
+        test = test.filter(n__gt=11)
+        self.assertEqual(test.count(), 89)
+
+        test = test.limit(10)
+        self.assertEqual(test.count(), 10)
+
+        Number.drop_collection()
+
+    def test_modify_evaulated_query_with_skip(self):
+        class Number(Document):
+            n = IntField()
+
+        Number.drop_collection()
+
+        for i in xrange(1, 101):
+            t = Number(n=i)
+            t.save()
+
+        test = Number.objects
+
+        self.assertEqual(test[0].n, 1)
+
+        test.skip(5)
+        self.assertEqual(test[0].n, 6)
+
+        Number.drop_collection()
+
     def test_unset_reference(self):
         class Comment(Document):
             text = StringField()