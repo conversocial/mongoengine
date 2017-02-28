conversocial/mongoengine

commit 3d4b752bda9d21215001e3772954446b0b64752b

Fix limit behaviour

mongoengine/__init__.py
@@ -12,7 +12,7 @@ from signals import *
 __all__ = (document.__all__ + fields.__all__ + connection.__all__ +
            queryset.__all__ + signals.__all__)
 
-VERSION = (0, 6, 17)
+VERSION = (0, 6, 18)
 
 
 def get_version():

mongoengine/queryset.py
@@ -513,7 +513,7 @@ class QuerySet(object):
                 self.order_by(*self._document._meta['ordering'])
 
             if self._limit is not None:
-                self._cursor_obj.limit(self._limit - (self._skip or 0))
+                self._cursor_obj.limit(self._limit)
 
             if self._skip is not None:
                 self._cursor_obj.skip(self._skip)

commit 0ce5ac0eb9dd93a5912e68ab9ddb6f02b823993a

fix: noticket - fix broken pagination + version bump 0.6.20
3d4b752bda9d21215001e3772954446b0b64752b introduced a bug that caused
the following test to fail:

FAIL: test_pagination (tests.django_tests.QuerySetTest)
Ensure that Pagination works as expected
----------------------------------------------------------------------
Traceback (most recent call last):
  File
    "/home/vagrant/conv_libs/mongoengine/tests/django_tests.py",
    line 92, in test_pagination
      self.assertEqual(t.render(Context(d)), u'%d:%d:' % (start, end))
  AssertionError: u'3:4:5:6:' != u'3:4:'

Only in some very rare cases (that the Django Paginator exercised) was
paging/slicing broken. I couldn't get a reproducable test case that
showed the same behaviour outside of the Django Paginator :(

mongoengine/__init__.py
@@ -12,7 +12,7 @@ from signals import *
 __all__ = (document.__all__ + fields.__all__ + connection.__all__ +
            queryset.__all__ + signals.__all__)
 
-VERSION = (0, 6, 19)
+VERSION = (0, 6, 20)
 
 
 def get_version():

mongoengine/queryset.py
@@ -1022,6 +1022,8 @@ class QuerySet(object):
             try:
                 self._cursor_obj = self._cursor[key]
                 self._skip, self._limit = key.start, key.stop
+                if key.start and key.stop:
+                    self._limit = key.stop - key.start
             except IndexError, err:
                 # PyMongo raises an error if key.start == key.stop, catch it,
                 # bin it, kill it.
@@ -1665,10 +1667,10 @@ class QuerySet(object):
         if self._limit is None:
             stop = start + limit
         if self._limit is not None:
-            if self._limit - start > limit:
+            if self._limit > limit:
                 stop = start + limit
             else:
-                stop = self._limit
+                stop = start + self._limit
         try:
             data = list(self[start:stop])
         except pymongo.errors.InvalidOperation: